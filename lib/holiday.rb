344409f1e1c2c13a62217407d434e4c134cb0120
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-26 16:16:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holidays, data|
-      puts "  #{holidays.to_s}: #{data.join(", ")}"
+      puts "  #{holidays.to_s.capitalize}: #{data.join(", ")}"
     end
   end
   
