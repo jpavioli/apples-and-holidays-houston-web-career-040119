012c613adb4abe8fb4b4fe39b6234d83528bf55e
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-26 18:20:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season,holiday|
     holiday.each do |array|
       array.include? "BBQ" 
-      puts holiday_hash[:season][:holiday]
+      puts holiday
     end
   end
 end
