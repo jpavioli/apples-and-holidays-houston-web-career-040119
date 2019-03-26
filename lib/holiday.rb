0974d32fe030b7dc618960d54c19b29de2dabf31
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-26 16:20:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holidays, data|
       nice_formatting = holidays.to_s.split("_")
       nice_formatting.each do |i|
-        i.capitalize
+        i.capitalize!
       end
       puts "  #{nice_formatting.join(" ")}: #{data.join(", ")}"
     end
